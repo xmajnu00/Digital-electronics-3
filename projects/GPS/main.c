0ba30399232646644e93774ad907176f75bd6c7a
Nolwenn Violette <lab661@PC-444.urel.feec.vutbr.cz>
2019-11-27 11:52:36 +0100

    projekt

---
+++ projects/GPS/main.c
@@ -0,0 +1,77 @@
+/*
+ * ---------------------------------------------------------------------
+ * Author:      Tomas Fryza
+ *              Dept. of Radio Electronics, Brno Univ. of Technology
+ * Created:     2018-10-23
+ * Last update: 2019-11-01
+ * Platform:    ATmega328P, 16 MHz, AVR 8-bit Toolchain 3.6.2
+ * ---------------------------------------------------------------------
+ * Description:
+ *    Analog-to-digital conversion with displaying result on LCD and 
+ *    transmitting via UART.
+ * 
+ * Note:
+ *    Peter Fleury's UART library.
+ */
+
+/* Includes ----------------------------------------------------------*/
+#include <stdlib.h>             // itoa() function
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include "timer.h"
+#include "lcd.h"
+#include "uart.h"
+
+/* Typedef -----------------------------------------------------------*/
+/* Define ------------------------------------------------------------*/
+#define UART_BAUD_RATE 9600
+
+
+/* Variables ---------------------------------------------------------*/
+/* Function prototypes -----------------------------------------------*/
+
+/* Functions ---------------------------------------------------------*/
+/**
+ *  Brief:  Main program. Read ADC result and transmit via UART.
+ *  Input:  None
+ *  Return: None
+ */
+int main(void)
+{
+    TIM_config_prescaler(TIM1, TIM_PRESC_64);       //OBNOVOVACI
+    TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
+
+    // UART: asynchronous, 8-bit data, no parity, 1-bit stop
+    uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
+
+    // Enables interrupts by setting the global interrupt mask
+    sei();
+s
+    // Put string to ringbuffer for transmitting via UART.
+    uart_puts("UART testing\r\n");
+
+    // Infinite loop
+    for (;;) {
+    }
+
+    // Will never reach this
+    return (0);
+}
+
+
+ISR(TIMER1_OVF_vect)
+{
+
+    uint16_t value = 0;
+    char uart_string[4];
+
+  
+    value = uart_getc();  //nefunguje
+
+    itoa(value ,uart_string , 10);
+   
+    uart_puts("\n");
+    uart_puts("\r");
+    uart_puts(uart_string);
+}
+
