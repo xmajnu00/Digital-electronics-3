77f67c41784d18b20ce8f77001c5581d9dbd14e0
Nolwenn Violette <lab661@PC-444.urel.feec.vutbr.cz>
2019-12-04 12:00:39 +0100

    nefunguje

--- projects/GPS/main.c
+++ projects/GPS/main.c
@@ -1,20 +1,3 @@
-/*
- * ---------------------------------------------------------------------
- * Author:      Tomas Fryza
- *              Dept. of Radio Electronics, Brno Univ. of Technology
- * Created:     2018-10-23
- * Last update: 2019-11-01
- * Platform:    ATmega328P, 16 MHz, AVR 8-bit Toolchain 3.6.2
- * ---------------------------------------------------------------------
- * Description:
- *    Analog-to-digital conversion with displaying result on LCD and 
- *    transmitting via UART.
- * 
- * Note:
- *    Peter Fleury's UART library.
- */
-
-/* Includes ----------------------------------------------------------*/
 #include <stdlib.h>             // itoa() function
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -27,31 +10,23 @@
 #define UART_BAUD_RATE 9600
 
 
-/* Variables ---------------------------------------------------------*/
-/* Function prototypes -----------------------------------------------*/
-
-/* Functions ---------------------------------------------------------*/
-/**
- *  Brief:  Main program. Read ADC result and transmit via UART.
- *  Input:  None
- *  Return: None
- */
 int main(void)
 {
     TIM_config_prescaler(TIM1, TIM_PRESC_64);       //OBNOVOVACI
     TIM_config_interrupt(TIM1, TIM_OVERFLOW_ENABLE);
 
-    // UART: asynchronous, 8-bit data, no parity, 1-bit stop
+    // // UART: asynchronous, 8-bit data, no parity, 1-bit stop
     uart_init(UART_BAUD_SELECT(UART_BAUD_RATE, F_CPU));
 
-    // Enables interrupts by setting the global interrupt mask
+    // // Enables interrupts by setting the global interrupt mask
     sei();
-s
-    // Put string to ringbuffer for transmitting via UART.
+
+    // // Put string to ringbuffer for transmitting via UART.
     uart_puts("UART testing\r\n");
 
     // Infinite loop
     for (;;) {
+           
     }
 
     // Will never reach this
@@ -59,19 +34,23 @@ s
 }
 
 
-ISR(TIMER1_OVF_vect)
-{
+ ISR(TIMER1_OVF_vect)
+  {
 
-    uint16_t value = 0;
-    char uart_string[4];
+      //uint16_t value = 0;
+      //char uart_string[4];
+    //   char znak;
 
   
-    value = uart_getc();  //nefunguje
+    //   znak = uart_getc();  
 
-    itoa(value ,uart_string , 10);
+    //   //itoa(value ,uart_string,16);
    
-    uart_puts("\n");
-    uart_puts("\r");
-    uart_puts(uart_string);
-}
+    //  if (znak == '$'){
+    //   uart_puts("\n");
+    //   uart_puts("\r");
+    //   }
+     
+    //   uart_putc(znak);
+  }
 
