8839ba52804b461c0b8f6eb550077c82e5ee45e6
Nolwenn Violette <lab661@PC-444.urel.feec.vutbr.cz>
2019-12-17 12:00:38 +0100

    knihovna upravena

--- projects/library/src/GPS_parsing.c
+++ projects/library/src/GPS_parsing.c
@@ -10,7 +10,7 @@ void  latitude_parse_print(volatile double lat, volatile char latitude_hemispher
 {   
     double fractpart; double intpart;
     int fp; int ip;
-    char vypis[50];
+    char extract[50];
 
     int degrees = lat/100;
     double seconds = (lat/100-degrees)*100;
@@ -25,11 +25,11 @@ void  latitude_parse_print(volatile double lat, volatile char latitude_hemispher
         nokia_lcd_write_char('-', 1);
     }
 
-    sprintf(vypis,"%d",ip);
-    nokia_lcd_write_string(vypis, 1);
+    sprintf(extract,"%d",ip);
+    nokia_lcd_write_string(extract, 1);
     nokia_lcd_write_char('.', 1);    
-    sprintf(vypis,"%d",fp);
-    nokia_lcd_write_string(vypis, 1);  
+    sprintf(extract,"%d",fp);
+    nokia_lcd_write_string(extract, 1);  
 
     nokia_lcd_render();  
 }
@@ -39,7 +39,7 @@ void  longitude_parse_print(volatile double lon, volatile char longitude_hemisph
 {   
     double fractpart; double intpart;
     int fp; int ip;
-    char vypis[50];
+    char extract[50];
 
     int degrees = lon/100;
     double seconds = (lon/100-degrees)*100;
@@ -54,32 +54,32 @@ void  longitude_parse_print(volatile double lon, volatile char longitude_hemisph
         nokia_lcd_write_char('-', 1);
     }
 
-    sprintf(vypis,"%d",ip);
-    nokia_lcd_write_string(vypis, 1);
+    sprintf(extract,"%d",ip);
+    nokia_lcd_write_string(extract, 1);
     nokia_lcd_write_char('.', 1);    
-    sprintf(vypis,"%d",fp);
-    nokia_lcd_write_string(vypis, 1);  
+    sprintf(extract,"%d",fp);
+    nokia_lcd_write_string(extract, 1);  
 
     nokia_lcd_render();  
 }
 
 
 void  time_parse_print(volatile double time){
-    char vypis[50];
+    char extract[50];
 
     int hour = time/10000;
     int min = (time/10000 - hour)*100;
     // int sec = time;
     // sec = sec%10;
 
-    sprintf(vypis,"%d",hour);
-    nokia_lcd_write_string(vypis, 1);
+    sprintf(extract,"%d",hour);
+    nokia_lcd_write_string(extract, 1);
     nokia_lcd_write_char(':', 1); 
-    sprintf(vypis,"%d",min);
-    nokia_lcd_write_string(vypis, 1);
+    sprintf(extract,"%d",min);
+    nokia_lcd_write_string(extract, 1);
     // nokia_lcd_write_char(':', 1);
-    // sprintf(vypis,"%d",sec);
-    // nokia_lcd_write_string(vypis, 1);
+    // sprintf(extract,"%d",sec);
+    // nokia_lcd_write_string(extract, 1);
 
     nokia_lcd_render(); 
 }
